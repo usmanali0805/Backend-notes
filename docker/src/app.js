ff5e15bebfecd4f86b37a20cdeaf35153ff24831
usmanali0805 <usman.ali08052005@gmail.com>
2026-07-29 23:51:09 +0500

    Create Docker image

---
+++ docker/src/app.js
@@ -0,0 +1,31 @@
+// import express from "express";
+// import { configDotenv } from "dotenv";
+// configDotenv()
+
+// const app = express ()
+
+// app.get('/user', (req, res)=>{
+//     res.status(200).json({
+//         status : true,
+//         message : "users fetch successfully"
+//     })
+// })
+
+// app.post('/auth', (req, res)=>{
+//     res.status(200).json({
+//         status : true,
+//         message : "users signup successfully"
+//     })
+// })
+
+// app.get('/health', (req, res)=>{
+//     res.status(200).json({
+//         status : true,
+//         message : "health api runing successfully"
+//     })
+// })
+
+// app.listen(process.env.PORT , ()=>{
+//     console.log(`Server is running on port no ${process.env.PORT}`)
+// })
+
