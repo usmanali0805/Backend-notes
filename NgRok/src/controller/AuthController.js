911af11208afe0c990e8b0c9fb5ce2101fd591e8
usmanali0805 <usman.ali08052005@gmail.com>
2026-08-16 00:47:34 +0500

    Added Login Functionality with Jsonwebjson

--- NgRok/src/controller/AuthController.js
+++ NgRok/src/controller/AuthController.js
@@ -2,11 +2,6 @@ import User from "../modules/Usermodel.js"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 
-const LoginFunc = (req, res) => {
-    const token = jwt.sign({ email, username }, process.env.JWT_SECRET_KEY);
-    console.log(token)
-
-}
 const SignupFunc = async (req, res) => {
     try {
         const { email, username, password } = req.body
@@ -26,7 +21,49 @@ const SignupFunc = async (req, res) => {
         })
 
     } catch (error) {
-       return res.status(400).json(error.message)
+        return res.status(400).json(error.message)
     }
 }
+
+const LoginFunc = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                status: false,
+                message: "Please fill all required fields"
+            })
+        }
+        const user = await User.findOne({ email })
+        const { username } = user
+        if (!user) {
+            return res.status(400).json({
+                status: false,
+                message: "User Not found"
+            })
+        }
+        await bcrypt.compare(password, user.password, async function (err, result) {
+            if (result) {
+                const token = jwt.sign({ email, username }, process.env.JWT_SECRET_KEY);
+                return res.status(200).json({
+                    status: true,
+                    message: "User Login Successfully",
+                    data: user,
+                    token: token
+                })
+            }
+            return res.status(400).json({
+                status: false,
+                message: "Invalid Credentails "
+            });
+        })
+    } catch (error) {
+        return res.status(400).json({
+            status: false,
+            message: error.message
+        })
+
+    }
+}
+
 export { LoginFunc, SignupFunc }
