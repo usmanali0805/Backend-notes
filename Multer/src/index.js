0d58294fda4223a067212b533e6fd3324059d906
usmanali0805 <usman.ali08052005@gmail.com>
2026-08-19 12:13:19 +0500

    Advance Advanced Multer in expressJS

--- Multer/src/index.js
+++ Multer/src/index.js
@@ -1,11 +1,35 @@
 import express from "express";
 import dotenv from "dotenv"
 import multer from "multer";
+import path from 'path'
 
 const app = express()
-const upload = multer({ dest: 'uploads/' })
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, "uploads/");
+//     },
+
+//     filename: (req, file, cb) => {
+//         const date = new Date()
+//         const uniqueName =
+//             date.getDate() + '-' + date.getMonth() + '-' +
+//             Math.round(Math.random() * 1e9) + file.originalname;
+
+//         cb(null, uniqueName);
+//     },
+// });
+
+
+const storage = multer.memoryStorage()
+const upload = multer({
+    storage,
+    limits: {
+        fileSize: 1 * 1024 * 1024
+    }
+});
 
 app.use(express.json())
+
 
 app.get('/user', (req, res) => {
     return res.status(200).json({
@@ -14,14 +38,19 @@ app.get('/user', (req, res) => {
     })
 })
 
-app.post('/profilePicture',upload.single('upload'),(req ,res)=>{
-    console.log(req.file)
-    if(req.file.mimetype !== 'image/png'){
-        return res.status(400).json({
-            status :false ,
-            message : "Only png images can be uploaded"
-        })
-    }
+app.post('/profilePicture', upload.single('upload'), (req, res) => {
+    const allowedTypes = [
+        "image/jpeg",
+        "image/png",
+        "image/webp",
+    ];
+    console.log(req.originalname)
+    // if(req.files.mimetype !== 'image/png'){
+    //     return res.status(400).json({
+    //         status :false ,
+    //         message : "Only png images can be uploaded"
+    //     })
+    // }
     return res.status(200).json("File is uploaded")
 })
 
